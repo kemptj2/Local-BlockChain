8497f19d028b6a829d1a19443f4c378d0d663903
kemptj2 <80777427+kemptj2@users.noreply.github.com>
2021-09-14 17:53:16 -0400

    Create scrip.py

---
+++ scrip.py
@@ -0,0 +1,15 @@
+from blockchain import Blockchain
+//Creation of local blocks
+block_one_transactions = {"sender":"Alice", "receiver": "Bob", "amount":"50"}
+block_two_transactions = {"sender": "Bob", "receiver":"Cole", "amount":"25"}
+block_three_transactions = {"sender":"Alice", "receiver":"Cole", "amount":"35"}
+fake_transactions = {"sender": "Bob", "receiver":"Cole, Alice", "amount":"25"}
+//Actions taken in the chain
+local_blockchain = Blockchain()
+local_blockchain.print_blocks()
+local_blockchain.add_block(block_one_transactions)
+local_blockchain.add_block(block_two_transactions)
+local_blockchain.add_block(block_three_transactions)
+local_blockchain.print_blocks()
+local_blockchain.chain[1].transactions = fake_transactions
+local_blockchain.validate_chain()
